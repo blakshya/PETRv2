d2f75a4ffd0fdc2ef5dbea7af02055b2ba5b0102
Lakshya Bhatnagar <bhatnagarlakshya3@gmail.com>
2025-04-26 15:27:11 -0700

    refactor

--- tools/build-dataset.py
+++ tools/build-dataset.py
@@ -1,8 +1,11 @@
 from data_converter.nuscenes_converter_seg import  create_nuscenes_infos
 
-
-
 if __name__ == '__main__':
     # Training settings
-    create_nuscenes_infos( '/data/Dataset/nuScenes/','HDmaps-final')
+    data_root = '/data/Dataset/nuScenes/'
+    info_prefix = 'HDmaps-final'
+    version='v1.0-trainval'
+    # version == 'v1.0-test'
+    # version == 'v1.0-mini'
+    create_nuscenes_infos(data_root, info_prefix, version)
 
